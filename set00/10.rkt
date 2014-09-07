466fc7d176a18f0a56ab766ac0fa6c795a90c7b9
harshs811 <harshs@ccs.neu.edu>
2014-09-07 17:26:10 -0400

    Change to Ex 10

--- set00/10.rkt
+++ set00/10.rkt
@@ -3,14 +3,20 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |10|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
 ; addition : Number Number Number -> Number
 ; GIVEN : three numbers
-; RETURNS : the sum of the two larger numbers
+; RETURNS : sum of the two larger numbers
 ; Examples:
 ; (addition 45 67 32) => 112
 ; (addition -1 0 2)   => 2
 ; (addition 7 2 4)    => 11
 
-(define (addition a b c)
- (if (= a (min a b c)) (+ b c) (if (= b (min a b c))(+ a c)
-                                   (+ a b))))
+(define (add-largest a b c)
+  (cond
+    [(= a (min a b c))
+        (+ b c)]    
+    [(= b (min a b c))
+     (+ a c)]
+    [else (+ a b)]
+    ))
 
-(check-expect (addition 1 0 0) 1)
+(check-expect (add-largest 10 5 4) 15)
+(check-expect (add-largest -1 0 2) 2)
